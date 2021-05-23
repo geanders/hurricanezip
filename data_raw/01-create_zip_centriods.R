fd2aed3e37a7c9a3849b1d3c1b330fb2d50f9657
Brooke Anderson <brooke.anderson@colostate.edu>
2021-05-22 18:43:15 -0600

    Add several years of study ZIP dfs

--- data_raw/01-create_zip_centriods.R
+++ data_raw/01-create_zip_centriods.R
@@ -33,180 +33,6 @@ zc_starts <- zc_centers %>%
   pull(zip_start) %>%
   unique()
 
-########################################################################
-
-# Get all ZIP codes that start with these codes as of 2018
-study_zips <- zctas(cb = TRUE, starts_with = zc_starts, year = 2018)
-
-# Change to a projection to calculate centroids then change back to lat-long
-study_zip_centroids <- study_zips %>%
-  st_transform(crs = 2163) %>%
-  st_centroid() %>%
-  st_transform(., "+proj=longlat +ellps=WGS84 +no_defs")
-
-# Convert to a dataframe of lat-long values for each ZIP code
-study_zips_df_2018 <- study_zip_centroids %>%
-  rename(postal_code = ZCTA5CE10) %>%
-  select(postal_code, geometry) %>%
-  as_tibble() %>%
-  mutate(long = unlist(map(.$geometry,1)),
-         lat = unlist(map(.$geometry,2))) %>%
-  select(-geometry)
-
-# Read in timezone shapefiles
-# Time zone shapefiles are from
-tz_sf <- st_read("data_raw/timezones.shapefile/combined-shapefile.shp") %>%
-  filter(tzid %in% c("America/Chicago",
-                     "America/Detroit",
-                     "America/Indiana/Indianapolis",
-                     "America/Indiana/Knox",
-                     "America/Indiana/Marengo",
-                     "America/Indiana/Petersburg",
-                     "America/Indiana/Tell_City",
-                     "America/Indiana/Vevay",
-                     "America/Indiana/Vincennes",
-                     "America/Indiana/Winamac",
-                     "America/Kentucky/Louisville",
-                     "America/Kentucky/Monticello",
-                     "America/New_York"))
-
-# Simplify the resolution of the borders to make object size
-# a bit smaller
-tz_sf_2 <- tz_sf %>%
-  st_transform(2163) %>%
-  st_simplify(dTolerance = 1000)
-
-# Next step: determine the time zone for each ZIP based on centroid
-
-use_data(study_zips_df_2018, overwrite = TRUE)
-write_csv(study_zips_df_2018,
-          file = "for_james/zip_code_centers_2018.csv")
-
-########################################################################
-
-# Get all ZIP codes that start with these codes as of 2017
-study_zips <- zctas(cb = TRUE, starts_with = zc_starts, year = 2017)
-
-# Change to a projection to calculate centroids then change back to lat-long
-study_zip_centroids <- study_zips %>%
-  st_transform(crs = 2163) %>%
-  st_centroid() %>%
-  st_transform(., "+proj=longlat +ellps=WGS84 +no_defs")
-
-# Convert to a dataframe of lat-long values for each ZIP code
-study_zips_df_2017 <- study_zip_centroids %>%
-  rename(postal_code = ZCTA5CE10) %>%
-  select(postal_code, geometry) %>%
-  as_tibble() %>%
-  mutate(long = unlist(map(.$geometry,1)),
-         lat = unlist(map(.$geometry,2))) %>%
-  select(-geometry)
-
-use_data(study_zips_df_2017, overwrite = TRUE)
-write_csv(study_zips_df_2017,
-          file = "for_james/zip_code_centers_2017.csv")
-
-
-##############################################################################
-
-# Get all ZIP codes that start with these codes as of 2019
-study_zips <- zctas(cb = TRUE, starts_with = zc_starts, year = 2019)
-
-# Change to a projection to calculate centroids then change back to lat-long
-study_zip_centroids <- study_zips %>%
-  st_transform(crs = 2163) %>%
-  st_centroid() %>%
-  st_transform(., "+proj=longlat +ellps=WGS84 +no_defs")
-
-# Convert to a dataframe of lat-long values for each ZIP code
-study_zips_df_2019 <- study_zip_centroids %>%
-  rename(postal_code = ZCTA5CE10) %>%
-  select(postal_code, geometry) %>%
-  as_tibble() %>%
-  mutate(long = unlist(map(.$geometry,1)),
-         lat = unlist(map(.$geometry,2))) %>%
-  select(-geometry)
-
-use_data(study_zips_df_2019, overwrite = TRUE)
-write_csv(study_zips_df_2019,
-          file = "for_james/zip_code_centers_2019.csv")
-
-##############################################################################
-
-# Get all ZIP codes that start with these codes as of 2016
-study_zips <- zctas(cb = TRUE, starts_with = zc_starts, year = 2016)
-
-# Change to a projection to calculate centroids then change back to lat-long
-study_zip_centroids <- study_zips %>%
-  st_transform(crs = 2163) %>%
-  st_centroid() %>%
-  st_transform(., "+proj=longlat +ellps=WGS84 +no_defs")
-
-# Convert to a dataframe of lat-long values for each ZIP code
-study_zips_df_2016 <- study_zip_centroids %>%
-  rename(postal_code = ZCTA5CE10) %>%
-  select(postal_code, geometry) %>%
-  as_tibble() %>%
-  mutate(long = unlist(map(.$geometry,1)),
-         lat = unlist(map(.$geometry,2))) %>%
-  select(-geometry)
-
-use_data(study_zips_df_2016, overwrite = TRUE)
-write_csv(study_zips_df_2016,
-          file = "for_james/zip_code_centers_2016.csv")
-
-##############################################################################
-
-## Code to check data
-
-study_state_sf <- states(cb = TRUE, resolution = "20m", year = 2018) %>%
-  filter(NAME %in% study_states)
-
-# Takes a while to run with all the ZIP codes!
-# ggplot() +
-#  geom_sf(data = study_state_sf, fill = "aliceblue") +
-#  geom_sf(data = study_zips, fill = "red", color = "red")
-
-# Check a subset of ZIP codes
-ggplot() +
-  geom_sf(data = filter(study_zips,
-                        str_sub(ZCTA5CE10, 1, 2) == "23"),
-          fill = "aliceblue", color = "red", size = 0.2) +
-  geom_sf(data = filter(study_zip_centroids,
-                        str_sub(ZCTA5CE10, 1, 2) == "23"),
-          color = "brown")
-
-# Compare to other resource for ZIP code centroid data
-zc_centers %>%
-  right_join(study_zips_df, by = "postal_code") %>%
-  ggplot(aes(x = longitude, y = long)) +
-  geom_point() +
-  geom_abline(slope = 1, intercept = 0)
-
-zc_centers %>%
-  right_join(study_zips_df, by = "postal_code") %>%
-  mutate(long_diff = abs(longitude - long)) %>%
-  arrange(desc(long_diff))
-
-# Checking the most different longitude point, it looks like the
-# `tigris` data is more correctly (this is our primary data)
-ggplot() +
-  geom_sf(data = filter(study_zips, ZCTA5CE10 == "79942"),
-          fill = "aliceblue", color = "red", size = 0.2) +
-  geom_sf(data = filter(study_zip_centroids, ZCTA5CE10 == "79942"),
-          color = "brown") +
-  geom_sf(data = st_as_sf(zc_centers,
-                          coords = c("longitude", "latitude"),
-                          crs = "+proj=longlat +ellps=WGS84 +no_defs") %>%
-            filter(postal_code == "79942"))
-
-
-zc_centers %>%
-  right_join(study_zips_df, by = "postal_code") %>%
-  ggplot(aes(x = latitude, y = lat)) +
-  geom_point() +
-  geom_abline(slope = 1, intercept = 0)
-
 ## Note: It looks like the `tigris` data on ZCTAs is better than the
 # dataset in the "US" folder. Right now, we're only using the "US"
 # data to get the prefixes for our study states, so we save time by
@@ -214,4 +40,120 @@ zc_centers %>%
 # right ZIP code prefixes for our states in a simpler dataset, it might
 # make sense to make that transition to use a simpler dataset and get
 # rid of the "US" file in the raw data folder.
+
+# Read in shapefiles for timezones in this part of the country
+tz_sf <- st_read("data_raw/timezones.shapefile/") %>%
+  filter(tzid %in% c("America/Chicago", "America/Denver",
+                     "America/Detroit", "America/Indiana/Indianapolis",
+                     "America/Indiana/Knox", "America/Indiana/Marengo",
+                     "America/Monterrey",
+                     "America/Indiana/Petersburg", "America/Indiana/Tell_City",
+                     "America/Indiana/Vevay", "America/Indiana/Vincennes",
+                     "America/Indiana/Winamac", "America/Kentucky/Louisville",
+                     "America/Kentucky/Monticello", "America/Menominee",
+                     "America/New_York")) %>%
+  st_transform(., "+proj=longlat +ellps=WGS84 +no_defs")
+
+
+########################################################################
+
+make_year_zcta_centers <- function(zcta_year){
+  # Get all ZIP codes that start with these codes as of the year
+  # Other than 2012, there's a lower-resolution version that's faster to process.
+  if(zcta_year == 2012){
+    study_zips <- zctas(starts_with = zc_starts, year = zcta_year)
+  } else {
+    study_zips <- zctas(cb = TRUE, starts_with = zc_starts, year = zcta_year)
+  }
+
+  # Change to a projection to calculate centroids then change back to lat-long
+  study_zip_centroids <- study_zips %>%
+    st_transform(crs = 2163) %>%
+    st_centroid() %>%
+    st_transform(., "+proj=longlat +ellps=WGS84 +no_defs")
+
+  # Convert to a dataframe of lat-long values for each ZIP code
+  if(zcta_year == 2000){
+    study_zips_df_year <- st_join(study_zip_centroids, tz_sf, join = st_within) %>%
+      rename(postal_code = ZCTA) %>%
+      select(postal_code, tzid, geometry) %>%
+      as_tibble() %>%
+      mutate(long = unlist(map(.$geometry,1)),
+             lat = unlist(map(.$geometry,2))) %>%
+      select(-geometry) %>%
+      filter(!str_detect(postal_code, "[A-Z]"))
+    # From the US Census' info on ZCTAs: "For the Census 2000 ZCTAs the Census Bureau
+    # created ZCTAs that ended in "XX" to represent large areas of land without
+    # ZIP Codes or "HH" to represent large areas of water without ZIP Codes. For the
+    # 2010 Census, large water bodies and large unpopulated land areas do not have ZCTAs."
+  } else {
+    study_zips_df_year <- st_join(study_zip_centroids, tz_sf, join = st_within) %>%
+      rename(postal_code = ZCTA5CE10) %>%
+      select(postal_code, tzid, geometry) %>%
+      as_tibble() %>%
+      mutate(long = unlist(map(.$geometry,1)),
+             lat = unlist(map(.$geometry,2))) %>%
+      select(-geometry)
+
+  }
+
+  return(study_zips_df_year)
+}
+
+# Sounds like there are no ZCTAs before 2000
+
+study_zips_df_2000 <- make_year_zcta_centers(zcta_year = 2000)
+use_data(study_zips_df_2000, overwrite = TRUE)
+write_csv(study_zips_df_2000,
+          file = "for_james/zip_code_centers_2000.csv")
+
+# Several additional years here
+
+study_zips_df_2010 <- make_year_zcta_centers(zcta_year = 2010)
+use_data(study_zips_df_2010, overwrite = TRUE)
+write_csv(study_zips_df_2010,
+          file = "for_james/zip_code_centers_2010.csv")
+
+# 2011
+
+study_zips_df_2012 <- make_year_zcta_centers(zcta_year = 2012)
+use_data(study_zips_df_2012, overwrite = TRUE)
+write_csv(study_zips_df_2012,
+          file = "for_james/zip_code_centers_2012.csv")
+
+study_zips_df_2013 <- make_year_zcta_centers(zcta_year = 2013)
+use_data(study_zips_df_2013, overwrite = TRUE)
+write_csv(study_zips_df_2013,
+          file = "for_james/zip_code_centers_2013.csv")
+
+study_zips_df_2014 <- make_year_zcta_centers(zcta_year = 2014)
+use_data(study_zips_df_2014, overwrite = TRUE)
+write_csv(study_zips_df_2014,
+          file = "for_james/zip_code_centers_2014.csv")
+
+study_zips_df_2015 <- make_year_zcta_centers(zcta_year = 2015)
+use_data(study_zips_df_2015, overwrite = TRUE)
+write_csv(study_zips_df_2015,
+          file = "for_james/zip_code_centers_2015.csv")
+
+study_zips_df_2016 <- make_year_zcta_centers(zcta_year = 2016)
+use_data(study_zips_df_2016, overwrite = TRUE)
+write_csv(study_zips_df_2016,
+          file = "for_james/zip_code_centers_2016.csv")
+
+study_zips_df_2017 <- make_year_zcta_centers(zcta_year = 2017)
+use_data(study_zips_df_2017, overwrite = TRUE)
+write_csv(study_zips_df_2017,
+          file = "for_james/zip_code_centers_2017.csv")
+
+study_zips_df_2018 <- make_year_zcta_centers(zcta_year = 2018)
+use_data(study_zips_df_2018, overwrite = TRUE)
+write_csv(study_zips_df_2018,
+          file = "for_james/zip_code_centers_2018.csv")
+
+study_zips_df_2019 <- make_year_zcta_centers(zcta_year = 2019)
+use_data(study_zips_df_2019, overwrite = TRUE)
+write_csv(study_zips_df_2019,
+          file = "for_james/zip_code_centers_2019.csv")
+
 
